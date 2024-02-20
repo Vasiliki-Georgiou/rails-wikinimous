7122e348f1c52e0aba24d337ad46f02f2c3cc9a7
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-20 20:57:58 +0100

    Adding style to exercise

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get "articles/new", to: "articles#new"
   post "articles", to: "articles#create"
   get "articles/:id", to: "articles#show", as: :article
-  get "articles/:id/edit", to: "articles#edit"
+  get "articles/:id/edit", to: "articles#edit", as: :article_edit
   patch "articles/:id", to: "articles#update"
   delete "articles/:id", to: "articles#destroy"
 end
